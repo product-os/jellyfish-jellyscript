71ae0c88e9b2a5b3cac8fa080aa52841f63066cb
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-03-14 12:46:45 +0000

    Drop transformers specific formulas
    These formulas should be added at the implementation layer, in the jellyfish-worker package.
    
    Change-type: major
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- lib/index.ts
+++ lib/index.ts
@@ -17,12 +17,6 @@ import staticEval from 'static-eval';
 import { FormulaPath, getFormulasPaths } from './card';
 import { reverseLink } from './link-traversal';
 
-enum NEEDS_STATUS {
-	PENDING = 'pending',
-	MERGEABLE = 'mergeable',
-	NEVER = 'never',
-}
-
 // TS-TODO: The esprima @types package doesn't include a definition for 'parse',
 // so we've manually defined it here.
 // Ideally the return type should be 'esprima.Program | esprima.Script'.
@@ -67,63 +61,6 @@ baseFormulas.AGGREGATE = <T>(list: any[], path: string, initial: any): T[] => {
 	const result: any = _.uniq(_.without(values, undefined));
 
 	return result;
-};
-
-baseFormulas.NEEDS_ALL = (...statuses: NEEDS_STATUS[]) => {
-	let result = NEEDS_STATUS.MERGEABLE;
-
-	for (const status of statuses) {
-		if (status === NEEDS_STATUS.NEVER) {
-			result = NEEDS_STATUS.NEVER;
-			break;
-		}
-
-		if (status === NEEDS_STATUS.PENDING) {
-			result = NEEDS_STATUS.PENDING;
-		}
-	}
-
-	return result;
-};
-
-/**
- * Looks at a contract and checks whether there is a backflow that meets the type and
- * filter callback (optional). If no, it means a transformer is still running and status
- * is pending. If yes, then if there is an error for the expected type, it concludes
- * it is never mergeable, otherwise if no error exists it is mergeable.
- *
- * @param contract Contract to look for a backflow
- * @param type The expexted output type of the backflow
- * @param func A filter function to match the backflow contract
- * @returns NEEDS_STATUS status
- */
-baseFormulas.NEEDS = (
-	contract: any,
-	type: string,
-	func: (contract: any) => boolean = () => true,
-) => {
-	const backflowHasError = contract.data.$transformer.backflow.some((c) => {
-		return (
-			c.type.split('@')[0] === 'error' &&
-			c.data.expectedOutputTypes.includes(type) &&
-			func(c)
-		);
-	});
-	if (backflowHasError) {
-		return NEEDS_STATUS.NEVER;
-	}
-
-	const backflowisMergeable = contract.data.$transformer.backflow.some(
-		(c) =>
-			c.type.split('@')[0] === type &&
-			func(c) &&
-			[NEEDS_STATUS.MERGEABLE, true].includes(c.data.$transformer.mergeable),
-	);
-	if (backflowisMergeable) {
-		return NEEDS_STATUS.MERGEABLE;
-	}
-
-	return NEEDS_STATUS.PENDING;
 };
 
 export interface Options {
